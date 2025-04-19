98803fe885f88f8a2a4947b17ef99129fabbf284
Handryo Casali <handryo@hotmail.com>
2025-04-19 16:40:15 -0300

    refactor s3

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "s3" {
-    bucket = "terraform-state-fiapx"
+    bucket = "${data.aws_caller_identity.current.account_id}-terraform-state-fiapx"
     key    = "infra-core/terraform.tfstate"
     region = "us-east-1"
   }
